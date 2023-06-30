f997f19f6a7077742c099072eec00050f445dcc8
Elemnur Güner <134098472+elemnurguner@users.noreply.github.com>
2023-06-30 20:10:56 +0300

    test_project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace testproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double alis, karOran, satis;
+            Console.Write("Sayıyı Girin : ");
+            alis = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Kar Oranını Girin : ");
+            karOran = Convert.ToDouble(Console.ReadLine());
+            satis = alis+(alis * karOran / 100);
+            Console.WriteLine("Satis Fiyati : {0}",satis);
+            Console.ReadLine();
+            Console.ReadKey();
+
+        }
+    }
+}
+
